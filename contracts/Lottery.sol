91c82d3ffbcce927fb983656db2954ca3e4db875
Dani Ihza Farrosi <dani.videda@gmail.com>
2022-01-01 18:01:43 +0700

    implement enter and getEntraceFee

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -7,20 +7,41 @@ contract Lottery {
     address payable[] public players;
     uint256 public usdEntryFee;
     AggregatorV3Interface internal priceFeed;
+    address owner;
 
     constructor(address _priceFeedAddress) {
         usdEntryFee = 50 * (10**18);
         priceFeed = AggregatorV3Interface(_priceFeedAddress);
+        owner = msg.sender;
     }
 
-    function enter() public {
+    function enter() public payable {
         // $50 minimum to enter
+        require(msg.value >= getEntranceFee(), "not enough ETH");
         players.push(payable(msg.sender));
     }
 
-    function getEntranceFee() public view returns (uint256) {}
+    function getEntranceFee() public view returns (uint256) {
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
+        uint256 usdEthConversionRate = uint256(answer) *
+            (10**18 / (10**priceFeed.decimals()));
+        // 372600299482
+        uint256 precision = 10**18;
+        uint256 conversionResult = (usdEntryFee * precision) /
+            usdEthConversionRate;
+        // 13419205531909524 x 10^-18 = 0.013419205531909524
+        return uint256(conversionResult);
+    }
 
     function startLottery() public {}
 
     function endLottery() public {}
+
+    function forceWithdraw() public {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+    function getBalance() public view returns (uint256) {
+        return uint256(address(this).balance);
+    }
 }
--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,12 +1,19 @@
-from brownie import Lottery, accounts
+from brownie import Lottery, accounts, config
+from brownie.network import account
+from web3 import Web3
+
+PRIVATE_KEY = config["wallets"]["private_key"]
 
 
 def deploy_lottery():
-    account = accounts[0]
+    # account = accounts[0]
+    account = accounts.add(PRIVATE_KEY)
     lottery = Lottery.deploy(
         "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e", {"from": account}
     )
-    print(lottery.address)
+    print("Deployed!", lottery.address)
+
+    lottery.enter({"from": account, "value": Web3.toWei(0.01, "ether")})
 
 
 def main():
